8951aafda2de20f36191dfeed5104974f9f3596b
Bhaskar Das <dassbhaskar2002@gmail.com>
2023-11-23 20:35:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
